c828681f6211a9d3fb116fcf57f124ecde917702
VitoriaPaula <48971522+VitoriaPaula@users.noreply.github.com>
2019-04-18 17:16:34 -0300

    Update Login.java

--- RealNews/RealNews/src/servlets/Login.java
+++ RealNews/RealNews/src/servlets/Login.java
@@ -6,50 +6,54 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import br.usjt.projetoWeb.service.UsuarioService;
+
 import br.usjt.projetoWeb.model.Usuario;
+import br.usjt.projetoWeb.service.UsuarioService;
 
 /**
  * Servlet implementation class Login
  */
-@WebServlet("/Login.do")
+@WebServlet("Login.do")
 public class Login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    
 	UsuarioService usuarioService;
-	
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Login() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * Default constructor.
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public Login() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		usuarioService = new UsuarioService();
-		
+
 		String email = request.getParameter("email");
 		String senha = request.getParameter("senha");
-		
+
 		Usuario usuario = usuarioService.consultar(email);
-		
-		if (senha.equals(usuario.getSenha())) {
-			response.sendRedirect("index.html"); //logado
+
+		if (senha.equals(usuario.getSenha()) && email.equals(usuario.getEmail())) {
+			response.sendRedirect("index.html"); // logado
 		} else {
-			request.getRequestDispatcher("login.html"); //volta a pagina inicial
+			request.getRequestDispatcher("login.html"); // volta a pagina inicial
 		}
+
 	}
 
 }
