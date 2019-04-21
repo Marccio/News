2689a47bc072cc9f291522d0f7582699d9dee3d3
VitoriaPaula <48971522+VitoriaPaula@users.noreply.github.com>
2019-04-21 13:02:15 -0300

    Add files via upload

---
+++ RealNews/RealNews/src/servlets/CadastroUsuario.java
@@ -0,0 +1,51 @@
+package br.usjt.projetoWeb.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.usjt.projetoWeb.model.Usuario;
+import br.usjt.projetoWeb.service.UsuarioService;
+
+/**
+ * Servlet implementation class CadastroUsuario
+ */
+@WebServlet("/CadastroUsuario")
+public class CadastroUsuario extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CadastroUsuario() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		UsuarioService usuarioService = new UsuarioService();
+		
+		String email = request.getParameter("email");
+		String nome = request.getParameter("nome");
+		String senha = request.getParameter("senha");
+		String perfil = request.getParameter("perfil");
+		
+		Usuario usuario = new Usuario(email, nome, senha, perfil);
+		int idUsuario = usuarioService.cadastrar(usuario);
+	}
+
+}
