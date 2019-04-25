7f88388346264667c30bb69e04934c3415f87889
VitoriaPaula <48971522+VitoriaPaula@users.noreply.github.com>
2019-04-25 20:02:54 -0300

    Update NoticiaService.java

--- News/src/br/usjt/projetoWeb/service/NoticiaService.java
+++ News/src/br/usjt/projetoWeb/service/NoticiaService.java
@@ -1,30 +1,40 @@
 package br.usjt.projetoWeb.service;
 
+import java.util.ArrayList;
+
 import br.usjt.projetoWeb.DAO.NoticiaDAO;
 import br.usjt.projetoWeb.model.Noticia;
 
 public class NoticiaService {
-
+	
 	NoticiaDAO noticiaDAO;
-
+	
 	public NoticiaService() {
 		this.noticiaDAO = new NoticiaDAO();
 	}
-
+	
 	public int cadastrar(Noticia noticia) {
 		return this.noticiaDAO.cadastrar(noticia);
 	}
-
+	
 	public Noticia consultar(int id) {
 		return this.noticiaDAO.consultar(id);
 	}
-
+	
 	public void excluir(int id) {
 		this.noticiaDAO.excluir(id);
 	}
-
+	
 	public void alterar(Noticia noticia) {
 		this.noticiaDAO.alterar(noticia);
+	}
+	
+	public ArrayList<Integer> listarNoticias(){
+		return this.noticiaDAO.listarNoticias();
+	}
+	
+	public ArrayList<Integer> buscarNoticias(String titulo){
+		return this.noticiaDAO.buscarNoticia(titulo);
 	}
 
 }
