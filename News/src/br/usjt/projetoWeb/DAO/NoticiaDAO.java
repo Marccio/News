ec1936ab106c6e7c734f996b316bb0b0b0771306
VitoriaPaula <48971522+VitoriaPaula@users.noreply.github.com>
2019-04-25 19:58:56 -0300

    Update NoticiaDAO.java

--- News/src/br/usjt/projetoWeb/DAO/NoticiaDAO.java
+++ News/src/br/usjt/projetoWeb/DAO/NoticiaDAO.java
@@ -4,9 +4,13 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+
 import br.usjt.projetoWeb.model.Noticia;
 
 public class NoticiaDAO {
+	
+	private static ArrayList<Integer> repositorio = new ArrayList<>();
 
 	Connection conexao;
 
@@ -14,20 +18,20 @@ public class NoticiaDAO {
 		try {
 			this.conexao = ConnectionFactory.conectar();
 
-			String sql = "INSERT INTO noticia (titulo, resumo, texto, img) VALUES (?,?,?,?)";
+			String sql = "INSERT INTO noticia (titulo, resumo, texto) VALUES (?,?,?)";
 
 			PreparedStatement ps = this.conexao.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
 
 			ps.setString(1, noticia.getTitulo());
 			ps.setString(2, noticia.getResumo());
 			ps.setString(3, noticia.getTexto());
-			ps.setString(4, noticia.getImg());
 
 			ps.execute();
 
 			ResultSet rs = ps.getGeneratedKeys();
 			if (rs.next()) {
 				int idGerado = rs.getInt(1);
+				repositorio.add(idGerado);
 				return idGerado;
 			}
 		} catch (SQLException ex) {
@@ -38,26 +42,24 @@ public class NoticiaDAO {
 
 	public Noticia consultar(int id) {
 		try {
-			this.conexao = ConnectionFactory.conectar();
-
-			String sql = "SELECT * FROM noticia WHERE id=?";
-			PreparedStatement ps = this.conexao.prepareStatement(sql);
-
-			ps.setInt(1, id);
-			ResultSet rs = ps.executeQuery();
-
-			if (rs.next()) {
-				Noticia noticia = new Noticia();
-				noticia.setTitulo(rs.getString("titulo"));
-				noticia.setResumo(rs.getString("resumo"));
-				noticia.setTexto(rs.getString("texto"));
-				noticia.setImg(rs.getString("img"));
-
-				return noticia;
-			} else
-				return null;
-		} catch (SQLException ex) {
-			ex.printStackTrace();
+		this.conexao = ConnectionFactory.conectar();
+		
+		String sql = "SELECT * FROM noticia WHERE id=?";
+		PreparedStatement ps = this.conexao.prepareStatement(sql);
+		
+		ps.setInt(1, id);
+		ResultSet rs = ps.executeQuery();
+		
+		if(rs.next()) {
+			Noticia noticia = new Noticia();
+			noticia.setTitulo(rs.getString("titulo"));
+			noticia.setResumo(rs.getString("resumo"));
+			noticia.setTexto(rs.getString("texto"));
+			
+			return noticia;	
+		} else return null;
+		} catch(SQLException ex)	{
+		ex.printStackTrace();
 		}
 		return null;
 	}
@@ -69,6 +71,12 @@ public class NoticiaDAO {
 
 			ps.setInt(1, id);
 			ps.execute();
+			
+			for(int i=0; i<=repositorio.size(); i++) {
+				if(repositorio.get(i) == id) {
+				repositorio.remove(i);
+				}
+			}
 		} catch (SQLException ex) {
 			ex.printStackTrace();
 		}
@@ -76,19 +84,41 @@ public class NoticiaDAO {
 
 	public void alterar(Noticia noticia) {
 		try {
-			String sql = "UPDATE noticia SET titulo=?, resumo=?, texto=?, img=? WHERE id=?";
+			String sql = "UPDATE noticia SET titulo=?, resumo=?, texto=? WHERE id=?";
 			PreparedStatement ps = this.conexao.prepareStatement(sql);
 
 			ps.setString(1, noticia.getTitulo());
 			ps.setString(2, noticia.getResumo());
 			ps.setString(3, noticia.getTexto());
-			ps.setString(4, noticia.getImg());
-			ps.setInt(5, noticia.getIdNoticia());
+			ps.setInt(4, noticia.getIdNoticia());
 
 			ps.execute();
 		} catch (SQLException ex) {
 			ex.printStackTrace();
 		}
 	}
-
+	
+	public ArrayList<Integer> buscarNoticia(String titulo){
+		try {
+			String sql = "SELECT id FROM noticia WHERE ucase(titulo) LIKE '%?%'";
+			PreparedStatement ps = this.conexao.prepareStatement(sql);
+			
+			ps.setString(1, titulo);
+			
+			ResultSet rs = ps.executeQuery();
+			ArrayList<Integer> lista = new ArrayList<>();
+			while(rs.next()) {
+				lista.add(rs.getInt("id"));
+			}
+			return lista;
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+		return null;
+	}
+	
+	public ArrayList<Integer> listarNoticias() {
+		return repositorio;
+		
+	}
 }
